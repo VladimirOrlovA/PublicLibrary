d2cf3fcca3eefeeb5a0c998c427f0052f5433dcc
VladimirOrlov <Vladimir.Orlov.A@mail.ru>
2020-02-11 19:02:12 +0600

    Добавьте файлы проекта.

---
+++ PublicLibrary.lip/DBcontent.cs
@@ -0,0 +1,27 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicLibrary.lip
+{
+    public class SomeThinth
+    {
+        public string Path { get; set; } = @"C:\Temp\MyData.db";
+        public List<User> GetAllUsers()
+        {
+            List<User> users = new List<User>();
+
+            using (var db = new LiteDatabase(Path))
+            {
+                users = db.GetCollection<User>("User").FindAll().ToList();
+            }
+
+            return users;
+        }
+
+
+    }
+}
